19079c094c0ff10d59d984f4b6aeb04fcab9bc24
nicolelopezfeliz <nicole.lopez@live.se>
2020-12-11 10:39:51 +0100

    Initial Commit

---
+++ PassDataWithSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataWithSegue
+//
+//  Created by Nicole Lopez feliz on 2020-12-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
